97446e2472a014cf11290a2ee8d21cfb34d55b4d
yhcheng0806 <412183786@qq.com>
2021-08-24 19:16:13 +0800

    update

---
+++ public/template/static/js/9.9794a366.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinstagram=this.webpackJsonpinstagram||[]).push([[9],{88:function(e,t,s){"use strict";s.r(t);s(0);var a=s(2);t.default=function(){return Object(a.jsx)("div",{className:"bg-gray-background",children:Object(a.jsx)("div",{className:"mx-auto max-w-screen-lg",children:Object(a.jsx)("p",{className:"text-center text-2xl",children:"Not Found!"})})})}}}]);
+//# sourceMappingURL=9.9794a366.chunk.js.map
